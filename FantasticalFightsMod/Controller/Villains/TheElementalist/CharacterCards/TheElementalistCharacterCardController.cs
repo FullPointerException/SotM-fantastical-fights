5d4e72fae34eb6d2a6547f926b3bd7e972ec266c
FullPointerException <fullpointerxception@gmail.com>
2022-03-17 01:30:17 -0500

    Added visual studio project and Controller for The Elementalist

---
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/CharacterCards/TheElementalistCharacterCardController.cs
@@ -0,0 +1,30 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+namespace Fpe.TheElementalist
+{
+    public class TheElementalistCharacterCardController : VillainCharacterCardController
+    {
+        public TheElementalistCharacterCardController(Card card, TurnTakerController turnTakeController)
+            : base(card, turnTakeController)
+        {
+
+        }
+
+        public override bool CanBeDestroyed => this.CharacterCard.IsFlipped;
+
+        public override void AddSideTriggers()
+        {
+            // At the start of the Villain turn, if there are no glyphs in play, {TheElementalist} flips.
+            
+        }
+    }
+
+    
+	//"When {TheElementalist} is destroyed, he flips."
+  
+
+    //"Whenever {TheElementalist} flips to this side, restore him to full HP. Then destroy all glyphs in play. For each glyphs destroyed this way, the elementalist deals himself 10 damage of that glyph's type. Shuffle the villain trash into the villain deck.",
+	//"Whenever a glyph is played, play the top card of the villain deck.",
+	//"At the end of the villain turn, play the top card of the villain deck."
+}
---
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/CharacterCards/TheElementalistTurnTakerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+
+
+namespace Fpe.TheElementalist
+{
+    public class TheElementalistTurnTakerController : TurnTakerController
+    {
+        public TheElementalistTurnTakerController(TurnTaker turnTaker, GameController gameController)
+            : base(turnTaker, gameController)
+        {
+        }
+
+		// Search the villain deck for 10 glyph cards and put them into play. Shuffle the villain deck.
+        public override IEnumerator StartGame()
+        {
+            var glyphs = FindCardsWhere(c => c.DoKeywordsContain("glyph"));
+            IEnumerator playGlyphs = base.PutCardsIntoPlay(new LinqCardCriteria((Card c) => c.DoKeywordsContain("glyph")), 10, true);
+
+            if(base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(playGlyphs);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(playGlyphs);
+            }
+        }
+    }
+}
