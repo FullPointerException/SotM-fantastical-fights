58988bf55b87dc2e8e2a287902dfb066bf319722
FullPointerException <fullpointerxception@gmail.com>
2022-03-18 13:47:29 -0500

    Implemented HolyAspectCardController

--- FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/HolyAspectCardController.cs
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/HolyAspectCardController.cs
@@ -7,5 +7,13 @@ namespace Fpe.TheElementalist
 			: base(card, turnTakerController)
 		{
 		}
+
+		public override void AddTriggers()
+		{
+			// {HolyAspect} is immune to radiant damage.
+			AddImmuneToDamageTrigger((DealDamageAction action) => action.DamageType == DamageType.Radiant && action.Target == this.Card)
+			// {TheElementalist} is immune to radiant damage.
+			AddImmuneToDamageTrigger((DealDamageAction action) => action.DamageType == DamageType.Radiant && action.Target == this.CharacterCard)
+		}
 	}
 }
