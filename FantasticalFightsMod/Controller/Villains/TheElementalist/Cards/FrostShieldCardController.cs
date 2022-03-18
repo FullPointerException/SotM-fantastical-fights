dbb0e75d0402a65d0479f811365afa67a484c3b1
FullPointerException <fullpointerxception@gmail.com>
2022-03-18 12:30:05 -0500

    Implemented FrostShieldCardController

--- FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/FrostShieldCardController.cs
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/FrostShieldCardController.cs
@@ -7,5 +7,13 @@ namespace Fpe.TheElementalist
 			: base(card, turnTakerController)
 		{
 		}
+
+		public override void AddTriggers()
+		{
+			// {FrostShield} is immune to cold damage.
+			AddImmuneToDamageTrigger((DealDamageAction action) => action.DamageType == DamageType.Cold && action.Target == this.Card)
+			// {TheElementalist} is immune to cold damage.
+			AddImmuneToDamageTrigger((DealDamageAction action) => action.DamageType == DamageType.Cold && action.Target == this.CharacterCard)
+		}
 	}
 }
