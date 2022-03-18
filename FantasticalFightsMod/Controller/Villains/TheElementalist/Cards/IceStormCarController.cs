04d6680aaf745a731de9a33a7bb89d247f2270aa
FullPointerException <fullpointerxception@gmail.com>
2022-03-18 15:36:52 -0500

    Added IceStormCardController

--- FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/IceStormCarController.cs
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/IceStormCarController.cs
@@ -8,5 +8,47 @@ namespace Fpe.TheElementalist
 		{
 
 		}
+
+		pujblic override IEnumerator Play()
+		{
+			List<DealDamageAction> targetResults = new List<DealDamageAction>();
+
+			// {TheElementalist} deals the {H-2} hero characters with the highest HP 3 cold damage.
+			IEnumerator coroutine = base.DealDamageToHighestHP(base.CharacterCard, 1, (Card c) => c.IsHero && c.IsTarget && c.IsCharacter, (Card c) => 3, DamageType.Cold, numberOfTargets: Game.H - 2, storedResults: targetResults)
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
+
+			//If {FrostShield} is in play, characters dealt damage this way cannot use powers until the start of the villain turn.
+			bool isInPlay = this.GameController.IsCardInPlayAndNotUnderCard("FrostShield");
+
+			if(isInPlay)
+			{
+				foreach(DeadDamageAction t in targetResults)
+				{
+					if(t != null && t.Target != null && t.Target.IsHeroCharacterCard && t.DidDealDamage)
+					{
+						CannotUsePowersStatusEffect cannotUsePowers = new CannotUsePowersStatusEffect();
+						cannotUsePowers.TurnTakerCriteria.IsSpecificTurnTaker = t.Target.NativeDeck.OwnerTurnTaker;
+						cannotUsePowers.UntilStartOfNextTurn(base.TurnTaker);
+
+						coroutine = base.AddStatusEffect(cannotUsePowersStatusEffect);
+            			if (base.UseUnityCoroutines)
+            			{
+                			yield return base.GameController.StartCoroutine(coroutine);
+            			}
+            			else
+            			{
+                			base.GameController.ExhaustCoroutine(coroutine);
+            			}
+					}
+				}
+			}
+		}
 	}
 }
