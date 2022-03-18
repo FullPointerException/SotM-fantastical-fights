00623154d085c8e50d44cb4995bfba9a6a4a0788
FullPointerException <fullpointerxception@gmail.com>
2022-03-18 16:07:34 -0500

    Implemented MeteorRainCardController.cs

--- FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/MeteorRainCardController.cs
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/MeteorRainCardController.cs
@@ -6,7 +6,35 @@ namespace Fpe.TheElementalist
 		protected MeteorRainCardController(Card card, TurnTakerController turnTakerController)
 			: base(card, turnTakerController)
 		{
+		}
 
+		public override IEnumerator Play()
+		{
+			// {TheElementalist} deals each non-villain target {H} fire damage.
+			IEnumerator coroutine = DealDamage(base.CharacterCard, (Card c) => !c.IsVillain && c.IsTarget, 5, DamageType.Fire);
+			if(UseUnityCoroutines)
+			{
+				yield return this.GameController.StartCoroutine(coroutine);
+			}
+			else
+			{
+				this.GameController.ExhaustCoroutine(coroutine);
+			}
+
+			// If {FlameBarrier} is in play, {TheElementalist} deals each non-villain target {H} fire damage.
+			bool isInPlay = this.GameController.IsCardInPlayAndNotUnderCard("FlameBarrier");
+			if(isInPlay)
+			{
+				coroutine = DealDamage(base.CharacterCard, (Card c) => !c.IsVillain && c.IsTarget, 5, DamageType.Fire);
+				if(UseUnityCoroutines)
+				{
+					yield return this.GameController.StartCoroutine(coroutine);
+				}
+				else
+				{
+					this.GameController.ExhaustCoroutine(coroutine);
+				}
+			}
 		}
 	}
 }
