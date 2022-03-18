0804c847f33ffb389e43e6e802d1c21d86f066f3
FullPointerException <fullpointerxception@gmail.com>
2022-03-18 16:12:03 -0500

    Implemented MindCrushCardController

--- FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/MindCrushCardController.cs
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/MindCrushCardController.cs
@@ -6,7 +6,35 @@ namespace Fpe.TheElementalist
 		protected MindCrushCardController(Card card, TurnTakerController turnTakerController)
 			: base(card, turnTakerController)
 		{
+		}
 
+		public override IEnumerator Play()
+		{
+			// {TheElementalist} deals each non-villain target 1 psychic damage.
+			IEnumerator coroutine = DealDamage(base.CharacterCard, (Card c) => !c.IsVillain && c.IsTarget, 1, DamageType.Psychic);
+			if(UseUnityCoroutines)
+			{
+				yield return this.GameController.StartCoroutine(coroutine);
+			}
+			else
+			{
+				this.GameController.ExhaustCoroutine(coroutine);
+			}
+
+			// If {MindBlank} is in play, destroy all hero ongoing cards.
+			bool isInPlay = this.GameController.IsCardInPlayAndNotUnderCard("Antitoxin");
+			if(isInPlay)
+			{
+				coroutine = this.GameController.DestroyCards(DecisionMaker, new LinqCardCriterial((Card c) => c.IsInPlayAndHasGameText && IsOngoing(c)));
+				if(UseUnityCoroutines)
+				{
+					yield return this.GameController.StartCoroutine(coroutine);
+				}
+				else
+				{
+					this.GameController.ExhaustCoroutine(coroutine);
+				}
+			}
 		}
 	}
 }
