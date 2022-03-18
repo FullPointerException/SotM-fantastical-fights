2a7e46a3c31c33f48f3f3e9d55cc1c39a02942f8
FullPointerException <fullpointerxception@gmail.com>
2022-03-18 12:33:44 -0500

    Implemented GroundingCardController.cs

--- FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/GroundingCardController.cs
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/GroundingCardController.cs
@@ -7,5 +7,13 @@ namespace Fpe.TheElementalist
 			: base(card, turnTakerController)
 		{
 		}
+
+		public override void AddTriggers()
+		{
+			// {Grounding} is immune to lightning damage.
+			AddImmuneToDamageTrigger((DealDamageAction action) => action.DamageType == DamageType.Lightning && action.Target == this.Card)
+			// {TheElementalist} is immune to lightning damage.
+			AddImmuneToDamageTrigger((DealDamageAction action) => action.DamageType == DamageType.Lightning && action.Target == this.CharacterCard)
+		}
 	}
 }
