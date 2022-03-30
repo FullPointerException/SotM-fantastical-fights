598853152a7d52a72fb96718c2b220a86b09c169
Full Pointer Exception <FullPointerXception@gmail.com>
2022-03-30 11:05:05 -0500

    Implemented PrismaticGlyphsCardController

--- FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/PrismaticGlyphsCardController.cs
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/PrismaticGlyphsCardController.cs
@@ -8,7 +8,13 @@ namespace Fpe.TheElementalist
 		public PrismaticGlyphsCardController(Card card, TurnTakerController turnTakerController)
 			: base(card, turnTakerController)
 		{
+		}
 
+		public override AddTriggers()
+		{
+			// If any glyphs are in play, {TheElementalist} is immune to damage.
+			base.AddImmuneToDamageTrigger((DealDamageAction d) => d.Target == base.CharacterCard &&
+				base.FindCardsWhere((Card c) => c.DoKeywordsContain("glyph"), true, base.CharacterCard));
 		}
 	}
 }
