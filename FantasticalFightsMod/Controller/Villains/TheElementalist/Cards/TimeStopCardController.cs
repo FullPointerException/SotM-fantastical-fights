29ed49e01d8fd32a595bdb621accc87dda8c0821
Full Pointer Exception <FullPointerXception@gmail.com>
2022-03-30 12:06:43 -0500

    Implemented TimeStopCardController

--- FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/TimeStopCardController.cs
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/TimeStopCardController.cs
@@ -8,7 +8,21 @@ namespace Fpe.TheElementalist
 		public TimeStopCardController(Card card, TurnTakerController turnTakerController)
 			: base(card, turnTakerController)
 		{
+		}
 
+		public override IEnumerator Play()
+		{
+			// Play the top 2 cards of the villain deck.
+            IEnumerator coroutine = base.GameController.PlayTopCard(this.DecisionMaker, base.TurnTakerController,
+            	numberOfCards: 2, cardSource: base.GetCardSource);
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(coroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(coroutine);
+            }
 		}
 	}
 }
