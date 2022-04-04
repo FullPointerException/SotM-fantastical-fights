545a6f60c1897a9ad9203d48f29062e443cebad6
Full Pointer Exception <FullPointerXception@gmail.com>
2022-04-04 10:58:54 -0500

    Playtest showed timestop was way to strong, replaced with a wind spell

---
+++ FantasticalFightsMod/Controller/Villains/TheElementalist/Cards/SlashingGaleCardController.cs
@@ -0,0 +1,54 @@
+namespace Fpe.TheElementalist
+{
+    using System.Collections;
+    using System.Linq;
+    using Handelabra.Sentinels.Engine.Controller;
+    using Handelabra.Sentinels.Engine.Model;
+
+    public class SonicBoomCardController : CardController
+    {
+        public SonicBoomCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+        }
+
+        public override IEnumerator Play()
+        {
+            // {TheElementalist} deals each hero X+1 projectile damage, where X is the number of equipment cards they have in play.
+            var damageRoutine = this.DealDamage(
+                                    this.CharacterCard,
+                                    c => c.IsHeroCharacterCard && !c.IsIncapacitatedOrOutOfGame,
+                                    target => target.TurnTaker.PlayArea.Cards.Where(c => c.IsInPlay && this.IsEquipment(c)).Count(),
+                                    DamageType.Projectile);
+            if (this.UseUnityCoroutines)
+            {
+                yield return this.GameController.StartCoroutine(damageRoutine);
+            }
+            else
+            {
+                this.GameController.ExhaustCoroutine(damageRoutine);
+            }
+
+            // If {Windwall} is in play, shuffle each hero's trash into their deck.
+            bool isInPlay = this.GameController.IsCardInPlayAndNotUnderCard("Windwall");
+            bool advancedAndAnyGlyph = this.IsGameAdvanced && this.CharacterCard.IsFlipped && this.FindCardsWhere((Card c) => c.DoKeywordsContain("glyph")).Any();
+
+            if (isInPlay || advancedAndAnyGlyph)
+            {
+                var heroTurnTakers = ;
+                foreach (var hero in this.GameController.FindHeroTurnTakerControllers().Where(h => !h.IsIncapacitatedOrOutOfGame))
+                {
+                    var shuffleRoutine = this.ShuffleTrashIntoDeck(hero, false, false, this.GetCardSource());
+                    if (this.UseUnityCoroutines)
+                    {
+                        yield return this.GameController.StartCoroutine(damageRoutine);
+                    }
+                    else
+                    {
+                        this.GameController.ExhaustCoroutine(damageRoutine);
+                    }
+                }
+            }
+        }
+    }
+}
